42743c28e2c42888fe3820a36c979876be01673e
tzwm <wangjunzhe4444@gmail.com>
2013-10-31 05:33:01 +0800

    Load all texture of model.

--- src/com/cdi/navigation_3d/MainActivity.java
+++ src/com/cdi/navigation_3d/MainActivity.java
@@ -1,15 +1,32 @@
 package com.cdi.navigation_3d;
 
+import min3d.Shared;
+import min3d.Utils;
 import min3d.core.Object3dContainer;
 import min3d.core.RendererActivity;
 import min3d.parser.IParser;
 import min3d.parser.Parser;
-import min3d.vos.Color4;
 import min3d.vos.Light;
 import min3d.vos.LightType;
+import android.graphics.Bitmap;
 import android.util.Log;
 
 public class MainActivity extends RendererActivity {
+	private final String material2color[][] = {{"011", "8d8d8d"},
+								  {"012", "a7a7a7"},
+								  {"013", "ffffff"},
+								  {"014", "ffffff"},
+								  {"ele", "ffffff"},
+								  {"i1", "ff69e7"},
+								  {"i2", "ff00e1"},
+								  {"i3", "ff00e1"},
+								  {"i4", "ff00e1"},
+								  {"lab 1", "ffc8c8"},
+								  {"lab 2", "bbf5a7"},
+								  {"lab 3", "fff400"},
+								  {"lab 4", "a3e8ff"},
+								  {"worksh", "ffffff"}};
+	
 	private final float CAM_RADIUS_X = 40;
 	private final float CAM_RADIUS_Y = 40;
 	private final float CAM_RADIUS_Z = 40;
@@ -25,7 +42,7 @@ public class MainActivity extends RendererActivity {
         scene.lights().add(light);
 
         IParser parser = Parser.createParser(Parser.Type.MAX_3DS,
-                getResources(), "com.cdi.navigation_3d:raw/cdi", false);
+                getResources(), "com.cdi.navigation_3d:raw/cdi", true);
         parser.parse();
 
         monster = parser.getParsedObject();
@@ -37,22 +54,13 @@ public class MainActivity extends RendererActivity {
 
         monster.vertexColorsEnabled(true);
         monster.colorMaterialEnabled(true);
-
+        initTexture();
+        loadAllTexture();
+        
         monster.getChildByName("lab 3_149").textures().clear();
-//        monster.getChildByName("lab 3_149").textures().addById("squared_robot_arm");
-        monster.getChildByName("lab 3_149").colors().clear();
-        monster.getChildByName("lab 3_149").defaultColor(new Color4(124, 244, 0, 255));
-        monster.getChildByName("lab 3_149").colorMaterialEnabled(true);
-//        monster.getChildByName("lab 3_149").colors().add(new Color4(124, 244, 0, 255));
-        monster.getChildByName("lab 3_149").vertexColorsEnabled(false);
+        monster.getChildByName("lab 3_149").textures().addById("ff00e1");
 
-        monster.getChildByName("lab 4").textures().clear();
-        monster.getChildByName("lab 4").colors().clear();
-        monster.getChildByName("lab 4").defaultColor(new Color4(124, 244, 0, 255));
-//        monster.getChildByName("lab 4").colorMaterialEnabled(true);
-//        monster.getChildByName("lab 4").vertexColorsEnabled(true);
-
-
+        
 		scene.camera().target = monster.position();
 
         scene.camera().position.x = 0;
@@ -79,5 +87,67 @@ public class MainActivity extends RendererActivity {
 
 		degrees += ROTATION_SPEED;
 
+	}
+	
+	private void loadAllTexture() {
+		int numChildren = monster.numChildren();
+		for(int i = 0; i < numChildren; i++) {
+			String name = monster.getChildAt(i).name();
+			
+			Boolean o = false;
+			for(int j = 0; j < 14; j++) {
+				if(name.startsWith(material2color[j][0])) {
+					monster.getChildAt(i).textures().clear();
+					monster.getChildAt(i).textures().addById(material2color[j][1]);
+					o = true;
+					break;
+				}
+			}
+			if(!o) {
+				monster.getChildAt(i).textures().clear();
+				monster.getChildAt(i).textures().addById("ff00e1");
+			}
+		}
+		
+	}
+	
+	private void initTexture() {
+		Bitmap b;
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_8d8d8d);
+        Shared.textureManager().addTextureId(b, "8d8d8d");
+        b.recycle();
+        
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_a3e8ff);
+        Shared.textureManager().addTextureId(b, "a3e8ff");
+        b.recycle();
+
+        
+        b = Utils.makeBitmapFromResourceId(R.drawable.c_a7a7a7);
+        Shared.textureManager().addTextureId(b,"a7a7a7");
+        b.recycle();
+        
+        b = Utils.makeBitmapFromResourceId(R.drawable.c_bbf5a7);
+        Shared.textureManager().addTextureId(b,"bbf5a7");
+        b.recycle();
+        
+        b = Utils.makeBitmapFromResourceId(R.drawable.c_ff00e1);
+        Shared.textureManager().addTextureId(b,"ff00e1");
+        b.recycle();
+       
+        b = Utils.makeBitmapFromResourceId(R.drawable.c_ff69e7);
+        Shared.textureManager().addTextureId(b,"ff69e7");
+        b.recycle();
+        
+        b = Utils.makeBitmapFromResourceId(R.drawable.c_ffc8c8);
+        Shared.textureManager().addTextureId(b,"ffc8c8");
+        b.recycle();
+
+        b = Utils.makeBitmapFromResourceId(R.drawable.c_fff400);
+        Shared.textureManager().addTextureId(b,"fff400");
+        b.recycle();
+       
+        b = Utils.makeBitmapFromResourceId(R.drawable.c_ffffff);
+        Shared.textureManager().addTextureId(b,"ffffff");
+        b.recycle();
 	}
 }
