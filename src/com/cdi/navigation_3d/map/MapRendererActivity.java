7bb38254845d888b3161e7c3b18d45eabddc7bba
tzwm <wangjunzhe4444@gmail.com>
2013-11-01 20:54:50 +0800

    Add layer switching by moveUp or moveDown.

--- src/com/cdi/navigation_3d/map/MapRendererActivity.java
+++ src/com/cdi/navigation_3d/map/MapRendererActivity.java
@@ -21,23 +21,23 @@ import com.cdi.navigation_3d.alg.Arc;
 import com.cdi.navigation_3d.alg.Graph;
 import com.cdi.navigation_3d.ui.G;
 
-public class MapRendererActivity extends RendererActivity implements OnClickListener{
-	private final String MATERIAL2COLOR[][] = {{"011", "8d8d8d"},
-								  {"012", "a7a7a7"},
-								  {"013", "ffffff"},
-								  {"014", "ffffff"},
-								  {"ele", "ffffff"},
-//								  {"i1", "ff69e7"},
-//								  {"i2", "ff00e1"},
-//								  {"i3", "ff00e1"},
-//								  {"i4", "ff00e1"},
-								  {"lab 1", "ffc8c8"},
-								  {"lab 2", "bbf5a7"},
-								  {"lab 3", "fff400"},
-								  {"lab 4", "a3e8ff"},
-								  {"worksh", "ffffff"}};
-	private final String LABCOLOR[] = {"ffc8c8", "bbf5a7", "fff400", "a3e8ff"};
-	
+public class MapRendererActivity extends RendererActivity implements
+		OnClickListener {
+	private final String MATERIAL2COLOR[][] = {
+			{ "011", "8d8d8d" },
+			{ "012", "a7a7a7" },
+			{ "013", "ffffff" },
+			{ "014", "ffffff" },
+			{ "ele", "ffffff" },
+			// {"i1", "ff69e7"},
+			// {"i2", "ff00e1"},
+			// {"i3", "ff00e1"},
+			// {"i4", "ff00e1"},
+			{ "lab 1", "ffc8c8" }, { "lab 2", "bbf5a7" },
+			{ "lab 3", "fff400" }, { "lab 4", "a3e8ff" },
+			{ "worksh", "ffffff" } };
+	private final String LABCOLOR[] = { "ffc8c8", "bbf5a7", "fff400", "a3e8ff" };
+
 	private final float CAM_RADIUS_X = 40;
 	private final float CAM_RADIUS_Y = 40;
 	private final float CAM_RADIUS_Z = 40;
@@ -47,187 +47,215 @@ public class MapRendererActivity extends RendererActivity implements OnClickList
 	private Object3dContainer monster;
 	private List<Arc> resultsEdgeList;
 	private int currentView;
+	private int currentLayer;
 
-	
+	private float downY;
+
 	@Override
 	public void initScene() {
 		resultsEdgeList = Graph.nodes2arcs(G.result);
-		
-        Light light = new Light();
-        light.type(LightType.DIRECTIONAL);
-        light.position.setAll(300, 150, 150);
-        scene.lights().add(light);
+		currentView = 0;
+		currentLayer = 2;
 
-        IParser parser = Parser.createParser(Parser.Type.MAX_3DS,
-                getResources(), "com.cdi.navigation_3d:raw/cdi", true);
-        parser.parse();
-        
-        monster = parser.getParsedObject();
-        monster.scale().x = monster.scale().y = monster.scale().z  = .24f;
+		Light light = new Light();
+		light.type(LightType.DIRECTIONAL);
+		light.position.setAll(300, 150, 150);
+		scene.lights().add(light);
+
+		IParser parser = Parser.createParser(Parser.Type.MAX_3DS,
+				getResources(), "com.cdi.navigation_3d:raw/cdi", true);
+		parser.parse();
+
+		monster = parser.getParsedObject();
+		monster.scale().x = monster.scale().y = monster.scale().z = .24f;
 		scene.addChild(monster);
-		
-        monster.vertexColorsEnabled(true);
-        monster.colorMaterialEnabled(true);
-        initTexture();
-        loadAllTexture();
-        loadLabTexture(2);
-        updateMap();
-		
-        changeToView2();
+
+		monster.vertexColorsEnabled(true);
+		monster.colorMaterialEnabled(true);
+		initTexture();
+		loadAllTexture();
+		updateRoute();
+		loadLabTexture(2);
+
+		changeToView2();
 	}
-	
-	public void updateMap() {
+
+	public void updateRoute() {
 		Iterator iter = resultsEdgeList.iterator();
-		while(iter.hasNext()) {
-			Arc node = (Arc)iter.next();
-			monster.getChildByName(node.name).isVisible(true);
-			monster.getChildByName(node.name).textures().clear();
-			monster.getChildByName(node.name).textures().addById("ff00e1");
+		while (iter.hasNext()) {
+			Arc node = (Arc) iter.next();
+			if(node.name.charAt(1) == (char)(currentLayer + (int) '0')) {
+				monster.getChildByName(node.name).isVisible(true);
+				monster.getChildByName(node.name).textures().clear();
+				monster.getChildByName(node.name).textures().addById("ff00e1");
+			}
 		}
 	}
-	
+
 	private void changeToView1() {
 		currentView = 1;
-		monster.scale().x = monster.scale().y = monster.scale().z  = .23f;
-        monster.position().x = -12;
-        monster.position().y = -3;
-        monster.position().z = 0;
-        
-        scene.camera().position.setAll(-7, 30, -25);
+		monster.scale().x = monster.scale().y = monster.scale().z = .23f;
+		monster.position().x = -12;
+		monster.position().y = -3;
+		monster.position().z = 0;
+
+		scene.camera().position.setAll(-7, 30, -25);
 		scene.camera().target.setAll(0, 0, 0);
-		
-		monster.getChildByName("lab 3_055").isVisible(false);
-		monster.getChildByName("lab 3_149").isVisible(false);
-		monster.getChildByName("lab 3_190").isVisible(false);
 	}
 
 	private void changeToView2() {
 		currentView = 2;
-		monster.scale().x = monster.scale().y = monster.scale().z  = .23f;
-        monster.position().x = -10;
-        monster.position().y = -5;
-        monster.position().z = 0;
-        
-        scene.camera().position.setAll(0, 20, -40);
+		monster.scale().x = monster.scale().y = monster.scale().z = .23f;
+		monster.position().x = -10;
+		monster.position().y = -5;
+		monster.position().z = 0;
+
+		scene.camera().position.setAll(0, 20, -40);
 		scene.camera().target.setAll(0, 0, 0);
-		
+
 		monster.getChildByName("012").isVisible(false);
 	}
-	
+
 	private void changeToView3() {
 		currentView = 3;
-		monster.scale().x = monster.scale().y = monster.scale().z  = .27f;
-        monster.position().x = -12;
-        monster.position().y = -19;
-        monster.position().z = 0;
-        
-        scene.camera().position.setAll(0, 32, -3);
+		monster.scale().x = monster.scale().y = monster.scale().z = .27f;
+		monster.position().x = -12;
+		monster.position().y = -19;
+		monster.position().z = 0;
+
+		scene.camera().position.setAll(0, 32, -3);
 		scene.camera().target.setAll(0, 0, 0);
-		
-		monster.getChildByName("lab 3_055").isVisible(false);
-		monster.getChildByName("lab 3_149").isVisible(false);
-		monster.getChildByName("lab 3_190").isVisible(false);
-	}
-	
-	@Override
-	public void updateScene() {
-//		float radians = degrees * ((float)Math.PI / 180);
-//
-//		scene.camera().position.x = (float)Math.cos(radians) * CAM_RADIUS_X;
-//		scene.camera().position.y = (float)Math.sin(radians) * CAM_RADIUS_Y;
-//		scene.camera().position.z = (float)Math.sin(radians) * CAM_RADIUS_Z;
-//
-//		degrees += ROTATION_SPEED;
 	}
 
-	
+	@Override
+	public void updateScene() {
+		// float radians = degrees * ((float)Math.PI / 180);
+		//
+		// scene.camera().position.x = (float)Math.cos(radians) * CAM_RADIUS_X;
+		// scene.camera().position.y = (float)Math.sin(radians) * CAM_RADIUS_Y;
+		// scene.camera().position.z = (float)Math.sin(radians) * CAM_RADIUS_Z;
+		//
+		// degrees += ROTATION_SPEED;
+	}
+
 	private void loadLabTexture(int num) {
 		String lab = "lab " + num;
-		String lab_color = LABCOLOR[num-1];
-		
+		String lab_color = LABCOLOR[num - 1];
+
 		monster.textures().clear();
-		
+
 		int numChildren = monster.numChildren();
-		for(int i = 0; i < numChildren; i++) {
+		for (int i = 0; i < numChildren; i++) {
 			String name = monster.getChildAt(i).name();
-			
+
 			if (name.startsWith(lab)) {
+				monster.getChildAt(i).isVisible(true);
 				monster.getChildAt(i).textures().clear();
 				monster.getChildAt(i).textures().addById(lab_color);
-			}
-			else {
+			} else
 				monster.getChildAt(i).isVisible(false);
-			}
+		}
+
+		if (num == 3) {
+			monster.getChildByName("lab 3_149").isVisible(false);
+		}
+		if (num == 2) {
+			monster.getChildByName("lab 3_190").isVisible(false);
+		}
+		if (num == 1) {
+			monster.getChildByName("lab 2_151").isVisible(false);
 		}
 	}
-	
+
 	private void loadAllTexture() {
 		monster.textures().clear();
-		
+
 		int numChildren = monster.numChildren();
-		for(int i = 0; i < numChildren; i++) {
+		for (int i = 0; i < numChildren; i++) {
 			String name = monster.getChildAt(i).name();
-			
+
 			Boolean o = false;
-			for(int j = 0; j < 10; j++) {
-				if(name.startsWith(MATERIAL2COLOR[j][0])) {
+			for (int j = 0; j < 10; j++) {
+				if (name.startsWith(MATERIAL2COLOR[j][0])) {
+					monster.getChildAt(i).isVisible(true);
 					monster.getChildAt(i).textures().clear();
-					monster.getChildAt(i).textures().addById(MATERIAL2COLOR[j][1]);
+					monster.getChildAt(i).textures()
+							.addById(MATERIAL2COLOR[j][1]);
 					o = true;
 					break;
 				}
 			}
-			if(!o) {
-//				monster.getChildAt(i).textures().clear();
-//				monster.getChildAt(i).textures().addById("ff00e1");
+			if (!o) {
+				// monster.getChildAt(i).textures().clear();
+				// monster.getChildAt(i).textures().addById("ff00e1");
 				monster.getChildAt(i).isVisible(false);
 			}
 		}
 	}
-	
+
 	private void initTexture() {
 		Bitmap b;
 		b = Utils.makeBitmapFromResourceId(R.drawable.c_8d8d8d);
-        Shared.textureManager().addTextureId(b, "8d8d8d");
-        b.recycle();
-        
+		Shared.textureManager().addTextureId(b, "8d8d8d");
+		b.recycle();
+
 		b = Utils.makeBitmapFromResourceId(R.drawable.c_a3e8ff);
-        Shared.textureManager().addTextureId(b, "a3e8ff");
-        b.recycle();
+		Shared.textureManager().addTextureId(b, "a3e8ff");
+		b.recycle();
 
-        
-        b = Utils.makeBitmapFromResourceId(R.drawable.c_a7a7a7);
-        Shared.textureManager().addTextureId(b,"a7a7a7");
-        b.recycle();
-        
-        b = Utils.makeBitmapFromResourceId(R.drawable.c_bbf5a7);
-        Shared.textureManager().addTextureId(b,"bbf5a7");
-        b.recycle();
-        
-        b = Utils.makeBitmapFromResourceId(R.drawable.c_ff00e1);
-        Shared.textureManager().addTextureId(b,"ff00e1");
-        b.recycle();
-       
-        b = Utils.makeBitmapFromResourceId(R.drawable.c_ff69e7);
-        Shared.textureManager().addTextureId(b,"ff69e7");
-        b.recycle();
-        
-        b = Utils.makeBitmapFromResourceId(R.drawable.c_ffc8c8);
-        Shared.textureManager().addTextureId(b,"ffc8c8");
-        b.recycle();
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_a7a7a7);
+		Shared.textureManager().addTextureId(b, "a7a7a7");
+		b.recycle();
 
-        b = Utils.makeBitmapFromResourceId(R.drawable.c_fff400);
-        Shared.textureManager().addTextureId(b,"fff400");
-        b.recycle();
-       
-        b = Utils.makeBitmapFromResourceId(R.drawable.c_ffffff);
-        Shared.textureManager().addTextureId(b,"ffffff");
-        b.recycle();
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_bbf5a7);
+		Shared.textureManager().addTextureId(b, "bbf5a7");
+		b.recycle();
+
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_ff00e1);
+		Shared.textureManager().addTextureId(b, "ff00e1");
+		b.recycle();
+
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_ff69e7);
+		Shared.textureManager().addTextureId(b, "ff69e7");
+		b.recycle();
+
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_ffc8c8);
+		Shared.textureManager().addTextureId(b, "ffc8c8");
+		b.recycle();
+
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_fff400);
+		Shared.textureManager().addTextureId(b, "fff400");
+		b.recycle();
+
+		b = Utils.makeBitmapFromResourceId(R.drawable.c_ffffff);
+		Shared.textureManager().addTextureId(b, "ffffff");
+		b.recycle();
 	}
-	
-    @Override
-	public boolean onTouchEvent(MotionEvent event){
-		if(event.getAction() == MotionEvent.ACTION_UP) {
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			downY = event.getY();
+		}
+
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			if (downY - event.getY() >= 100) {
+				if (currentLayer == 4) {
+					return true;
+				}
+				loadLabTexture(++currentLayer);
+				updateRoute();
+				return true;
+			}
+			if (event.getY() - downY >= 100) {
+				if (currentLayer == 1) {
+					return true;
+				}
+				loadLabTexture(--currentLayer);
+				updateRoute();
+				return true;
+			}
+
 			switch (currentView) {
 			case 1:
 				changeToView2();
@@ -242,13 +270,13 @@ public class MapRendererActivity extends RendererActivity implements OnClickList
 				break;
 			}
 		}
-    	
-    	return true;
-    }
-    
+
+		return true;
+	}
+
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
